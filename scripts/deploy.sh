9f65fa33427aaaa67c2ccac01f5477c69929a18a
astephannie <astephannie@gmail.com>
2017-11-13 10:51:29 -0500

    Update .sh file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,14 +33,14 @@ else
 fi
 
 # copy over or recompile the new site
-cp -a "../${siteSource}/." .
+# cp -a "../${siteSource}/." .
 
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin gh-pages > /dev/null 2>&1
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
