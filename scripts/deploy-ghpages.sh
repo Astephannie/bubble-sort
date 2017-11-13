e199a73eea3eb0dd54cd4480f8dbee7a8d905be4
astephannie <astephannie@gmail.com>
2017-11-12 22:54:30 -0500

    Update .sh file

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 # ideas used from https://gist.github.com/motemen/8595451
 
-# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
-# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
-
 # abort the script if there is a non-zero error
 set -e
 
 # show where we are on the machine
 pwd
+
 remote=$(git config remote.origin.url)
+
+siteSource="$1"
+
+if [ ! -d "$siteSource" ]
+then
+    echo "Usage: $0 <site source dir>"
+    exit 1
+fi
 
 # make a directory to put the gp-pages branch
 mkdir gh-pages-branch
@@ -19,7 +25,6 @@ git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
-
 
 # switch into the the gh-pages branch
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
@@ -33,14 +38,15 @@ else
 fi
 
 # copy over or recompile the new site
-#cp -a "../${siteSource}/." .
+cp -a "../${siteSource}/." .
 
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin gh-pages > /dev/null 2>&1
+
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
